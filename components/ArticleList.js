6cc3863c0f9b4ce52a821e811f2027f879c78ad8
shahjalal rafi <shahjalalrafi5@gmail.com>
2021-06-28 21:30:15 +0600

    data fetching and showing complete

---
+++ components/ArticleList.js
@@ -0,0 +1,16 @@
+import articleStyles from '../styles/Article.module.css'
+import ArticleItem from './ArticleItem';
+
+const ArticleList = ({articles}) => {
+    return ( 
+        <div className={articleStyles.grid}>
+            {
+        articles.map(article => (
+          <ArticleItem key={article.id} article={article} />
+        ))
+      }
+        </div>
+     );
+}
+ 
+export default ArticleList;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import ArticleList from '../components/ArticleList'
 
-export default function Home() {
+export default function Home({articles}) {
   return (
     <div>
       <Head>
         <title>Home page</title>
         <meta name="viewport" content="web developer, application" />
       </Head>
-     <h2>Welcome to next</h2>
+      <ArticleList articles={articles} />
     </div>
   )
+}
+
+export const getStaticProps = async() => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
